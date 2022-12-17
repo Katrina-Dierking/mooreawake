ec120441db5fed3a3e47608bff5cd585a40182ac
Katrina <kalynndier@gmail.com>
2022-12-16 23:06:56 -0500

    added color boxes

---
+++ src/components/colorboxes/ColorBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './colorbox.css'
+
+const ColorBox = () => {
+  return (
+    <div className='colorBoxes'>
+        <div className='circle-1'><p>abc</p></div>
+        <div className='circle-2'><p>def</p></div>
+        <div className='circle-3'><p>ghi</p></div>
+    </div>
+  )
+}
+
+export default ColorBox
--- src/testpage/TestPage.js
+++ src/testpage/TestPage.js
@@ -2,40 +2,35 @@ import React from 'react'
 import { TopNav } from '../components/topNav/TopNav'
 import './testpage.css'
 import blueSwirl from './blue-swirl.png'
+import ColorBox from '../components/colorboxes/ColorBox'
 
 
 const TestPage = () => {
   return (
     <>
-      <div className="testBody">
-        <TopNav />
-      </div>
-      <section className="swirl">
-        <img src={blueSwirl} />
-      </section>
-        <section>
-              <div className="testVerse">
-                <p>
-                  And that, knowing the time, that now it is high time to awake out of
-                  sleep:
-                </p>
-                <p> for now is our salvation nearer than when we believed.</p>
-        
-                <p>~ Romans 13:11 ~</p>
-              </div>
-      </section>
+      <div className="pageBody">
+        <div className="testBody">
+          <TopNav />
+        </div>
+        <section className="swirl">
+          <img src={blueSwirl} />
+          </section>
+          <section>
+            <div className="testVerse">
+              <p>
+                And that, knowing the time, that now it is high time to <span className='awake'>Awake </span>
+                out of sleep:
+              </p>
+              <p> for now is our salvation nearer than when we believed.</p>
+
+              <p>~ Romans 13:11 ~</p>
+            </div>
+          </section>
+          <ColorBox />
+          </div>
     </>
   );
 }
 
 export default TestPage
 
-  // <section className="buttons">
-  //       <div>
-  //         <h3>Choose a location</h3>
-  //       </div>
-  //       <div className="choices">
-  //         <button>Online</button>
-  //         <h4> | </h4>
-  //         <h5>Carthage, NC</h5>
-  //       </div>
